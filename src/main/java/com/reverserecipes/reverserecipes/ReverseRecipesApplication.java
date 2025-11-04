b8b5e037e69fa85598f0849df839184b75111400
Pablo Soto <psoto@amplifire.com>
2025-11-04 15:05:31 -0300

    Initial commit

---
+++ src/main/java/com/reverserecipes/reverserecipes/ReverseRecipesApplication.java
@@ -0,0 +1,13 @@
+package com.reverserecipes.reverserecipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReverseRecipesApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(ReverseRecipesApplication.class, args);
+   }
+
+}
---
+++ src/test/java/com/reverserecipes/reverserecipes/ReverseRecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reverserecipes.reverserecipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReverseRecipesApplicationTests {
+
+   @Test
+   void contextLoads() {
+   }
+
+}
